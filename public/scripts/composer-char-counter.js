fa33593277f78c6a0618498370d0dcd729d54b89
Michael Dever <mdever@live.ca>
2021-12-15 01:55:56 -0500

    add container element for tweets. add and style tweet element

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  console.log("Ready!");
+  const $textarea = $('#tweet-text')
+  // $('.new-tweet form').blur(function() {
+  //   console.log(this);
+  // })
+  // $('.new-tweet form').keydown(function() {
+  //   console.log(this);
+  // })
+  // $('.new-tweet form').keyup(function() {
+  //   console.log(this);
+  // })
+  // $('.new-tweet form').keypress(function() {
+  //   const textLength = $(this).val().length;
+  //   console.log(textLength);
+    // console.log((('.new-tweet form').val()));
+  // })
+  // $('.new-tweet form').change(function() {
+  //   console.log(this);
+  // })
+  $textarea.on('input', function(event) {
+    const textLength = event.target.value.length;
+    console.log(textLength);
+    $('.counter').val(140 - textLength)
+    if (textLength > 140) {
+      $('.counter').css('color', 'red')
+    } else {
+      $('.counter').css('color', 'black')
+    }
+
+    // console.log(this);
+  })
+});
